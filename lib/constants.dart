55c14b49716fc64327c48d8a7e04b0eff7eabfa2
Felipe-dot <felipeluis610@gmail.com>
2024-07-15 19:03:36 -0300

    Add constant file

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkBrown = Color(0xFFC67C4E);
+  static const Color lightBrown = Color(0xFFEDD6C8);
+  static const Color black = Color(0xFF313131);
+  static const Color gray = Color(0xFFE3E3E3);
+  static const Color surface = Color(0xFFE3E3E3);
+}
